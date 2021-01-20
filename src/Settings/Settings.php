9fe7826442b1ca180ce8ee036e698364deaf6de6
Jan Barášek <janbarasek@gmail.com>
2021-01-20 09:58:51 +0100

    Settings: Fix PhpStan

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -83,8 +83,8 @@ final class Settings
 				->renderToString(__DIR__ . '/../../template/install-basic.latte', [
 					'basePath' => Url::get()->getBaseUrl(),
 					'locale' => $this->localization->getLocale(),
-					'isLocalhost' => $url === null || strpos($url, 'localhost') !== false,
-					'isBarajaCz' => $url !== null && strpos($url, 'baraja.cz') !== false,
+					'isLocalhost' => strpos($url, 'localhost') !== false,
+					'isBarajaCz' => strpos($url, 'baraja.cz') !== false,
 				]);
 		}
 
