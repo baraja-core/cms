95b739a0556a0e0cdbdf10c5aac4a8d612827a38
Jan Barášek <jan.barasek@packeta.com>
2021-05-17 16:57:40 +0200

    Announcement: Define new entity.

---
+++ src/Announcement/Entity/Announcement.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Cms\Announcement\Entity;
+
+
+use Baraja\Cms\User\Entity\User;
+use Baraja\Doctrine\Identifier\IdentifierUnsigned;
+use Baraja\Localization\Localization;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="cms__announcement")
+ */
+class Announcement
+{
+	use IdentifierUnsigned;
+
+	/** @ORM\ManyToOne(targetEntity="User", inversedBy="logins") */
+	private User $user;
+
+	/** @ORM\ManyToOne(targetEntity="Announcement", inversedBy="children") */
+	private ?self $parent = null;
+
+	/**
+	 * @var self[]|Collection
+	 * @ORM\OneToMany(targetEntity="Announcement", mappedBy="parent")
+	 */
+	private $children;
+
+	/** @ORM\Column(type="string", length=2, nullable=true) */
+	private ?string $locale = null;
+
+	/** @ORM\Column(type="text") */
+	private string $message;
+
+	/** @ORM\Column(type="datetime") */
+	private \DateTime $showSince;
+
+	/** @ORM\Column(type="datetime", nullable=true) */
+	private ?\DateTime $showUntil = null;
+
+	/** @ORM\Column(type="boolean") */
+	private bool $active = false;
+
+	/** @ORM\Column(type="boolean") */
+	private bool $pinned = false;
+
+
+	public function __construct(User $user, ?string $locale, string $message, ?self $parent = null)
+	{
+		$this->user = $user;
+		$this->locale = $locale ? Localization::normalize($locale) : null;
+		$this->setMessage($message);
+		$this->parent = $parent;
+		$this->showSince = new \DateTime;
+		$this->children = new ArrayCollection;
+	}
+
+
+	public function getUser(): User
+	{
+		return $this->user;
+	}
+
+
+	public function getParent(): ?Announcement
+	{
+		return $this->parent;
+	}
+
+
+	/**
+	 * @return self[]|Collection
+	 */
+	public function getChildren()
+	{
+		return $this->children;
+	}
+
+
+	public function getLocale(): ?string
+	{
+		return $this->locale;
+	}
+
+
+	public function getMessage(): string
+	{
+		return $this->message;
+	}
+
+
+	public function getShowSince(): \DateTime
+	{
+		return $this->showSince;
+	}
+
+
+	public function getShowUntil(): ?\DateTime
+	{
+		return $this->showUntil;
+	}
+
+
+	public function isActive(): bool
+	{
+		return $this->active;
+	}
+
+
+	public function isPinned(): bool
+	{
+		return $this->pinned;
+	}
+
+
+	public function setMessage(string $message): void
+	{
+		$message = trim($message);
+		if ($message === '') {
+			throw new \InvalidArgumentException('Message can not be empty.');
+		}
+		$this->message = $message;
+	}
+
+
+	public function setShowSince(\DateTime $showSince): void
+	{
+		$this->showSince = $showSince;
+	}
+
+
+	public function setShowUntil(?\DateTime $showUntil): void
+	{
+		$this->showUntil = $showUntil;
+	}
+
+
+	public function setActive(bool $active): void
+	{
+		$this->active = $active;
+	}
+
+
+	public function setPinned(bool $pinned): void
+	{
+		$this->pinned = $pinned;
+	}
+}
