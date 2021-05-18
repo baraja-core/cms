34aff8124a03dc9a834995c1f3f10913daa65647
Jan Barášek <janbarasek@gmail.com>
2021-05-18 09:27:41 +0200

    Sentry: Add native Sentry bridge to catch all internal admin exceptions.

---
+++ src/Bridge/SentryBridge.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Cms\MiddleWare\Bridge;
+
+
+use Baraja\AdminBar\User\AdminIdentity;
+use Baraja\Cms\Helpers;
+use Baraja\TracySentryBridge\SentryLogger;
+use Nette\Security\User;
+use Sentry\State\Scope;
+
+use function Sentry\configureScope;
+
+final class SentryBridge
+{
+	public function __construct(
+		private User $user,
+	) {
+	}
+
+
+	public function register(): void
+	{
+		if (class_exists(SentryLogger::class)) {
+			SentryLogger::register();
+		}
+		if (function_exists('Sentry\configureScope') === false) {
+			return;
+		}
+		configureScope(
+			function (Scope $scope): void {
+				if ($this->user->isLoggedIn() === false) {
+					return;
+				}
+				$identity = $this->user->getIdentity();
+				if ($identity instanceof AdminIdentity) {
+					$scope->setUser(
+						[
+							'id' => $identity->getId(),
+							'ip_address' => Helpers::userIp(),
+							'username' => $identity->getName(),
+							'roles' => $identity->getRoles(),
+						]
+					);
+				}
+			}
+		);
+	}
+}
